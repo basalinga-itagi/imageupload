9d181c2d69cf6c11e92c31127025d4099266fee8
user <basalingappa.itagi@inqude.com>
2023-10-11 13:37:21 +0530

    uploading multiple images

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import express from "express";
 import multer from "multer";
 const app = express();
 
-// const upload = multer({ dest: "uploads/" });
+// upload single file;
+
+const upload = multer({ dest: "uploads/" });
 // app.post("/upload", upload.single("file"), (req, res) => {
 //   res.send("uploading images while post");
 // });
+
+//uploading multiple images
+app.post("/upload", upload.array("file"), (req, res) => {});
 app.listen(8080, () => {
   console.log("listening on port 80880");
 });
