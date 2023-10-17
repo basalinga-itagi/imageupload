3c08b734cfb5ce273f6597397fdffe3f250f3f89
user <basalingappa.itagi@inqude.com>
2023-10-17 23:14:26 +0530

    Successfully Storing single image in aws

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import multer from "multer";
-// import uuid from "uuid/v4";
 import { v4 as uuidv4 } from "uuid";
-import S3 from "aws-sdk/clients/s3";
+import { uploadImage } from "./s3service.js";
+import AWS from "aws-sdk";
+import multerS3 from "multer-s3";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
+const s3 = new AWS.S3();
 // upload single file;
 
 // const upload = multer({ dest: "uploads/" });
@@ -29,32 +30,55 @@ function fileFilter(req, file, cb) {
 
 //custom file name
 // uuid-originalfilnamae
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    const { originalname } = file;
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     const { originalname } = file;
 
-    cb(null, `${uuidv4()}-${originalname}`);
-  },
-});
-const upload = multer({ storage, fileFilter });
+//     cb(null, `${uuidv4()}-${originalname}`);
+//   },
+// });
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
+
+// Configure multer to use S3 storage
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: process.env.AWS_BUCKET_NAME,
+//     key: function (req, file, cb) {
+//       cb(null, Date.now().toString() + "-" + file.originalname);
+//     },
+//   }),
+// });
+
+//memory. storage.
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 //uploading multiple images
-app.post("/upload", upload.array("file"), (req, res) => {
-  res.send("uploading images while post");
+app.post("/upload", upload.array("file"), async (req, res) => {
+  const files = req.files[0];
+  // console.log("files", files);
+  const result = await uploadImage(files);
+  // console.log("result", req.file.location);
+  res.json({ imageUrl: result });
 });
-app.use((error, req, res, next) => {
-  if (error instanceof multer.MulterError) {
-    // A Multer error occurred when uploading.
-    if (error.code === "LIMIT_UNEXPECTED_FILE") {
-      res.status(500).send("Send image type ");
-    }
-  } else if (err) {
-    // An unknown error occurred when uploading.
-  }
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+  res
+    .status(500)
+    .json({ error: "An error occurred while uploading the image" });
 });
+
 app.listen(8080, () => {
   console.log("listening on port 8080");
 });
--- s3service.js
+++ s3service.js
@@ -1,11 +1,30 @@
-import { S3 } from "aws-sdk";
+import AWS from "aws-sdk";
+import S3 from "aws-sdk/clients/s3.js";
 import { v4 as uuidv4 } from "uuid";
 
-uploadImage = async (file) => {
-  const s3 = new S3();
+export const uploadImage = async (file) => {
+  //   const { S3 } = pkg;
+
+  const bucketName = process.env.AWS_BUCKET_NAME;
+  const region = process.env.AWS_REGION;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secreatAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
+
+  const s3 = new S3({
+    region,
+    accessKeyId,
+    secreatAccessKey,
+  });
+
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
-    key: `uploads/${file.originalname}`,
+    Key: file.originalname,
     Body: file.buffer,
   };
   const result = await s3.upload(params).promise();
